bea92d1ba9043766736e2ff03eb6e096b5eaee64
Dave Sharp <dave@192-168-20-13.abcdefghijklmnopqrstuvwxyz012345.plex.direct>
2024-12-17 11:36:11 +0800

    Fix image loading and cleanup object URLs

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, 
+    static_folder='static',  # specify the static folder
+    template_folder='templates'  # specify the templates folder
+)
 
 @app.route('/')
 def index():
